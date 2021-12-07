7d38a3832a920544d9f1cd2586022f8bca44d48e
Cristian Alexander Martinez <40245786+AlxZeroX@users.noreply.github.com>
2021-12-06 20:23:11 -0600

    Update database.js

--- src/database.js
+++ src/database.js
@@ -8,7 +8,7 @@ const mongoose = require('mongoose')
 
 mongoose.set('useFindAndModify', false);
 //mongoose.connect(process.env.MONGODB_URI, {
-    mongoose.connect('mongodb+srv://camo:camo123@industria.s123y.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
+    mongoose.connect('mongodb+srv://camo:camo123@industria.s123y.mongodb.net/<dbname>?retryWrites=true&w=majority',{
     useUnifiedTopology: true,
     useNewUrlParser: true,
     useCreateIndex: true
